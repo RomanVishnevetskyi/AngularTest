7a9751673cc53394da4daefa5155f10e8d6dda6a
RomanVishnevetskyi <vrv080289@gmail.com>
2022-01-27 17:17:59 +0200

    work with routing

--- routing/src/app/app-routing/app-routing.module.ts
+++ routing/src/app/app-routing/app-routing.module.ts
@@ -6,17 +6,19 @@ import { PostsComponent } from "../posts/posts.component";
 import { PostComponent } from "../post/post.component";
 import { AboutExtraComponent } from "../about-extra/about-extra.component";
 import { ErrorComponent } from "../error/error.component";
+import { AuthGuard } from "../auth.guard";
 
 const routes: Routes = [
   { path: "", component: HomeComponent },
   {
     path: "about",
     component: AboutComponent,
+    canActivateChild: [AuthGuard],
     children: [{ path: "extra", component: AboutExtraComponent }],
   },
-  { path: "posts", component: PostsComponent },
+  { path: "posts", component: PostsComponent, canActivate: [AuthGuard] },
   { path: "posts/:id", component: PostComponent },
-  { path: "error", component: ErrorComponent },
+  { path: "**", component: ErrorComponent },
   { path: "**", redirectTo: "/error" },
 ];
 
